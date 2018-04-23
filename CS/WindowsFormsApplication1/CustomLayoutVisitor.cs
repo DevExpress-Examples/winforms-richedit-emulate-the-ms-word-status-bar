ad53c13d1a1b3021ed9ed8f170cdfbccc2ce760e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication1/CustomLayoutVisitor.cs
@@ -0,0 +1,99 @@
+﻿using DevExpress.XtraRichEdit.API.Layout;
+using DevExpress.XtraRichEdit.API.Native;
+using DevExpress.XtraRichEdit.Layout;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public class StaticsticsVisitor : IDocumentVisitor
+    {
+        readonly StringBuilder buffer;
+
+        public StaticsticsVisitor()
+        {
+            WordCount = 0;
+            this.buffer = new StringBuilder();
+        }
+
+        public virtual void Visit(DocumentText text)
+        {
+            Buffer.Append(text.Text);
+        }
+
+        public virtual void Visit(DocumentParagraphEnd paragraphEnd)
+        {
+            FinishParagraph();
+        }
+
+        void FinishParagraph()
+        {
+            string text = Buffer.ToString();
+            this.WordCount += text.Split(new char[] { ' ', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            Buffer.Length = 0;
+        }
+
+        public virtual void Visit(DocumentTableCellBorder cellBorder) { }
+        public virtual void Visit(DocumentSectionStart sectionStart) { }
+        public virtual void Visit(DocumentFieldCodeStart fieldCodeStart) { }
+        public virtual void Visit(DocumentFieldCodeEnd fieldCodeEnd) { }
+        public virtual void Visit(DocumentFieldResultEnd fieldResultEnd) { }
+        public virtual void Visit(DocumentBookmarkStart bookmarkStart) { }
+        public virtual void Visit(DocumentBookmarkEnd bookmarkEnd) { }
+        public virtual void Visit(DocumentCommentStart commentStart) { }
+        public virtual void Visit(DocumentSectionEnd sectionEnd) { }
+        public virtual void Visit(DocumentCommentEnd commentEnd) { }
+        public virtual void Visit(DocumentRangePermissionStart rangePermissionStart) { }
+        public virtual void Visit(DocumentRangePermissionEnd rangePermissionEnd) { }
+        public virtual void Visit(DocumentTextBox textBox) { }
+        public virtual void Visit(DocumentParagraphStart paragraphStart) { }
+        public virtual void Visit(DocumentInlinePicture inlinePicture) { }
+        public virtual void Visit(DocumentPicture picture) { }
+        public virtual void Visit(DocumentHyperlinkStart hyperlinkStart) { }
+        public virtual void Visit(DocumentHyperlinkEnd hyperlinkEnd) { }
+
+        public int WordCount { get; private set; }
+        protected StringBuilder Buffer { get { return buffer; } }
+    }
+
+    public class CustomLayoutVisitor : LayoutVisitor
+    {
+        Document document;
+
+        public CustomLayoutVisitor(Document doc)
+        {
+            this.document = doc;
+            RowIndex = 0;
+            ColIndex = 0;
+            IsFound = false;
+        }
+
+        protected override void VisitRow(LayoutRow row)
+        {
+            if (!IsFound)
+            {
+                RowIndex++;
+                if (row.Range.Contains(document.CaretPosition.ToInt()))
+                {
+                    IsFound = true;
+                    ColIndex = document.CaretPosition.ToInt() - row.Range.Start;
+                }
+            }
+            base.VisitRow(row);
+        }
+
+        public void Reset()
+        {
+            RowIndex = 0;
+            ColIndex = 0;
+        }
+
+        public int ColIndex { get; private set; }
+        public int RowIndex { get; private set; }
+        public bool IsFound { get; private set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,14 @@
+# How to emulate the MS Word information status bar when using RichEditControl
+
+
+<p>This example demonstrates how to implement an information status bar that mimics the MS Word status bar:<br><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-emulate-the-ms-word-information-status-bar-when-using-richeditcontrol-t467150/16.1.9+/media/150534eb-cd19-11e6-80bf-00155d62480c.png"><br><br></p>
+<p>This task is accomplished by using the RichEditControl's DocumentLayout API. Review the following help topic for additional information:<br><a href="https://documentation.devexpress.com/#WindowsForms/CustomDocument114069">Layout API</a><br><br></p>
+<p>In this example, we use different approaches to collect required information
+
+* The <a href="https://documentation.devexpress.com/CoreLibraries/clsDevExpressXtraRichEditAPINativeDocumentIteratortopic.aspx">DocumentIterator</a> class to obtain the total word count.
+* A custom <a href="https://documentation.devexpress.com/#CoreLibraries/clsDevExpressXtraRichEditAPILayoutLayoutVisitortopic">LayoutVisitor</a> descendant to get the caret position (the line and column indices).</p>
+<p><br><strong>See also:<br></strong><a href="https://www.devexpress.com/Support/Center/p/T245818">Layout API - Simple Example</a><br><a href="https://www.devexpress.com/Support/Center/p/T266080">Document Layout API - Practical usage</a></p>
+
+<br/>
+
+
